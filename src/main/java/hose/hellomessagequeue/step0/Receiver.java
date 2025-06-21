039dd0c05edc028f9124de9225b5331eafff87e7
hose Joo <hose0728@naver.com>
2025-06-21 17:21:21 +0900

    Initial commit: RabbitMQ configuration setup

---
+++ src/main/java/hose/hellomessagequeue/step0/Receiver.java
@@ -0,0 +1,10 @@
+package hose.hellomessagequeue.step0;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+    }
+}
---
+++ src/main/java/hose/hellomessagequeue/step0/Sender.java
@@ -0,0 +1,16 @@
+package hose.hellomessagequeue.step0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Sender {
+
+    private final RabbitTemplate rabbitTemplate;
+    public void send(String message) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_NAME, message);
+        System.out.println("Sent <" + message + ">");
+    }
+}
